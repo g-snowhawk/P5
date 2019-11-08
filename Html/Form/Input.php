4d9e54f0c85cf1f188f9a9a467edf1e0b032cb5a
g-snowhawk <githave@p5on.net>
2019-11-08 13:32:01 +0900

    Support date/datetime to setValue()

--- Html/Form/Input.php
+++ Html/Form/Input.php
@@ -32,12 +32,18 @@ class P5_Html_Form_Input
      */
     public static function setValue($fmObj, $html, $element, $type, $name, $value, $sec)
     {
-        if ($type == 'radio') {
+        if ($type === 'radio') {
             P5_Html_Form_Radio::setValue($fmObj, $html, $element, $value);
-        } elseif ($type == 'checkbox') {
+        } elseif ($type === 'checkbox') {
             P5_Html_Form_Checkbox::setValue($fmObj, $html, $element, $name, $value, $sec);
-        } elseif ($type == 'File') {
+        } elseif ($type === 'File') {
             P5_Html_Form_File::setValue($fmObj, $html, $element, $name, $value, $sec);
+        } elseif ($type === 'date') {
+            $value = date('Y-m-d', strtotime($value));
+            P5_Html_Form_Text::setValue($fmObj, $html, $element, $name, $value, $sec);
+        } elseif ($type === 'datetime') {
+            $value = date('Y-m-d\TH:i:s', strtotime($value));
+            P5_Html_Form_Text::setValue($fmObj, $html, $element, $name, $value, $sec);
         } else {
             P5_Html_Form_Text::setValue($fmObj, $html, $element, $name, $value, $sec);
         }
@@ -53,11 +59,11 @@ class P5_Html_Form_Input
      */
     public static function preview($fmObj, $html, $form, $element, $type, $name, $value, $sec)
     {
-        if ($type == 'radio') {
+        if ($type === 'radio') {
             P5_Html_Form_Radio::preview($fmObj, $html, $form, $element, $value);
-        } elseif ($type == 'checkbox') {
+        } elseif ($type === 'checkbox') {
             P5_Html_Form_Checkbox::preview($fmObj, $html, $form, $element, $name, $value);
-        } elseif ($type == 'file') {
+        } elseif ($type === 'file') {
             P5_Html_Form_File::preview($fmObj, $html, $form, $element, $type, $name, $value, $sec);
         } else {
             P5_Html_Form_Text::preview($fmObj, $html, $form, $element, $type, $name, $value, $sec);
