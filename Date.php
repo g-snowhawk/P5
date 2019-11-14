34ebc6c786ed0ba20cb76fff85475fb9f04dcbda
g-snowhawk <githave@p5on.net>
2019-11-14 17:00:33 +0900

    Add function of convert A.D. to Wareki

--- Date.php
+++ Date.php
@@ -77,4 +77,53 @@ class P5_Date
 
         return $a - time() > 0;
     }
+
+    public static function wareki($format, $timestamp = null)
+    {
+        if (is_null($timestamp)) {
+            $timestamp = time();
+        }
+
+        $year = (int)date('Y', $timestamp);
+        $gengo = '';
+        $gengo_short = '';
+        if ($timestamp < strtotime('1868-01-25')) {
+            $gengo = '';
+            $gengo_short = '';
+        }
+        // Meiji
+        elseif ($timestamp < strtotime('1912-07-30')) {
+            $gengo = mb_convert_encoding('&#26126;&#27835;', 'UTF-8', 'HTML-ENTITIES');
+            $gengo_short = 'M';
+            $year -= 1867;
+        }
+        // Taisho
+        elseif ($timestamp < strtotime('1926-12-25')) {
+            $gengo = mb_convert_encoding('&#22823;&#27491;', 'UTF-8', 'HTML-ENTITIES');
+            $gengo_short = 'T';
+            $year -= 1911;
+        }
+        // Showa
+        elseif ($timestamp < strtotime('1989-0l-08')) {
+            $gengo = mb_convert_encoding('&#26157;&#21644;', 'UTF-8', 'HTML-ENTITIES');
+            $gengo_short = 'S';
+            $year -= 1925;
+        }
+        // Heisei
+        elseif ($timestamp < strtotime('2019-05-01')) {
+            $gengo = mb_convert_encoding('&#24179;&#25104;', 'UTF-8', 'HTML-ENTITIES');
+            $gengo_short = 'H';
+            $year -= 1988;
+        }
+        // Reiwa
+        else {
+            $gengo = mb_convert_encoding('&#20196;&#21644;', 'UTF-8', 'HTML-ENTITIES');
+            $gengo_short = 'R';
+            $year -= 2018;
+        }
+        $wareki = str_replace(['Y','y'], ['Q','q'], $format);
+        $datestr = date($wareki, $timestamp);
+
+        return str_replace(['Q','q'], ["$gengo$year","$gengo_short$year"], $datestr);
+    }
 }
